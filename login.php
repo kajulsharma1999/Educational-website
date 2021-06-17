7da33bc50acbaec862792ba9d45e2a113e5367aa
Kajul Sharma <65329658+kajulsharma1999@users.noreply.github.com>
2021-06-17 13:53:42 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <style>
+        #login-form-teacher {
+            border: 2px ;
+            border-color: rgb(104, 145, 162);
+            padding: 25px 25px;
+            background-color: #333333;
+            color: #bcbcbc;
+            border-radius: 3px;
+            margin: 50px 275px 50px 275px;
+            /* width: auto; */
+        }
+        .btn {
+            background-color: #eea41;
+            border: 2px solid #ffffff;
+            border-color: white;
+            transition-duration: 0.4s;
+        }
+
+        .btn:hover {
+            background-color: #eea412;
+            color: white;
+        }
+        
+    </style>
+    <!-- Basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Site Metas -->
+    <title>Kaju Edu Site</title>
+    <!--Meta Keyword-->
+    <meta name="Keyword" content="Education | Tuition | Kaju | Study">
+    <!-- Meta Description-->
+    <meta name="description"
+        content="KajuEdu Site is an online teaching Portal where students find best teacher near home">
+    <!-- Meta Author-->
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/kajul.jpg" type="#" />
+    <link rel="apple-touch-icon" href="#">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- ALL VERSION CSS -->
+    <link rel="stylesheet" href="css/versions.css">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!-- Modernizer for Portfolio -->
+    <script src="js/modernizer.js"></script>
+</head>
+
+<body class="host_version">
+
+    <!-- Start header -->
+    <header class="top-navbar">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.html">
+
+                    <img src="images/kajulll.jpg" alt="" />
+                </a>
+                <div class="collapse navbar-collapse" id="navbars-host">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Course
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="dropdown-a">
+                                <a class="dropdown-item" href="course-grid-3.html">Course </a>
+                            </div>
+                        </li>
+                        <li class="nav-item active"><a class="nav-link" href="teachers.html">Teachers</a></li>
+                        <li class="nav-item"><a class="nav-link" href="Student.html">Student</a></li>
+                        <li class="nav-item"><a class="nav-link" href="pricing.html">Pricing</a></li>
+                        <li class="nav-item"><a class="nav-link" href="about.html">About Us</a></li>
+                        <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!-- End header -->
+
+    <div class="all-title-box">
+        <div class="container text-center">
+            <h1>Teachers<span class="m_1">My teachers</span></h1>
+        </div>
+    </div>
+
+    <div id="teachers" class="section wb">
+        <div class="container">
+            <div class="our-team">
+
+                <h1><b>Teacher SignIn</b></h1>
+                <form id="login-form-teacher">
+                    <div class="col-12">
+                       
+                        <input type="email" name="inputEmail" class="form-control" id="inputEmail4" placeholder="Email address" required>
+                    </div><br>
+                    <div class="col-12">
+                        
+                        <input type="password" name="password" class="form-control" id="inputPassword4"  placeholder="Password" required>
+                    </div><br>
+                    <div id="remember" class="checkbox">
+                    <label>
+                        <input type="checkbox" value="remember-me"> Remember me
+                    </label><br>
+                    </div>
+                    <div class="col-12">
+                    <button class="btn btn-lg btn-block" type="submit">Sign in</button>
+                    </div><br>
+                    <div class="col-12">
+                    <a href="#" class="forgot-password">
+                        Forgot the password?</a>
+                    </div>
+
+                </form>
+
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-xs-12">
+                    <div class="widget clearfix">
+                        <div class="widget-title">
+                            <h3>About US</h3>
+                        </div>
+                        <p> Kaju Edu is a leading online home tuition and private tutors, connecting teachers,
+                            parents and students together. We provide highly qualified home tutors at your door step to
+                            work with students of all ages and classes.</p>
+                        <div class="footer-right">
+                            <ul class="footer-links-soi">
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            </ul><!-- end links -->
+                        </div>
+                    </div><!-- end clearfix -->
+                </div><!-- end col -->
+
+                <div class="col-lg-4 col-md-4 col-xs-12">
+                    <div class="widget clearfix">
+                        <div class="widget-title">
+                            <h3>Information Link</h3>
+                        </div>
+                        <ul class="footer-links">
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="pricing.html">Pricing</a></li>
+                            <li><a href="about.html">About</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul><!-- end links -->
+                    </div><!-- end clearfix -->
+                </div><!-- end col -->
+
+                <div class="col-lg-4 col-md-4 col-xs-12">
+                    <div class="widget clearfix">
+                        <div class="widget-title">
+                            <h3>Contact Details</h3>
+                        </div>
+                        <ul class="footer-links">
+                            <li><a href="#">kajulsharma76.com</a></li>
+                            <li><a href="#">www.kajulsharma.com</a></li>
+                            <li>India</li>
+                            <li>+910000000000</li>
+                        </ul><!-- end links -->
+                    </div><!-- end clearfix -->
+                </div><!-- end col -->
+
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </footer><!-- end footer -->
+
+    <div class="copyrights">
+        <div class="container">
+            <div class="footer-distributed">
+                <div class="footer-center">
+                    <p class="footer-company-name">Developed by: <a href="index.html">kajulsharma</a></p>
+                </div>
+            </div>
+        </div><!-- end container -->
+    </div><!-- end copyrights -->
+
+    <a href="#" id="scroll-to-top" class="dmtop global-radius"><i class="fa fa-angle-up"></i></a>
+
+    <!-- ALL JS FILES -->
+    <script src="js/all.js"></script>
+    <!-- ALL PLUGINS -->
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
